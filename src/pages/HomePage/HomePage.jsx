304d3aac615da57361842aad76188a1b5eb0af8e
Arsen Akopyan <tigrikpictures@gmail.com>
2024-07-04 01:58:40 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,22 @@
 import { NavLink } from "react-router-dom";
 import css from "./HomePage.module.css";
 import { LogoIcon } from "../../images/icons";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <>
-      <NavLink className={css.titleLink} to="/contacts">
-        <div className={css.title}>
-          <LogoIcon />
-          <h1 className={css.titleText}>Contacts</h1>
-        </div>
-      </NavLink>
+      {isLoggedIn && (
+        <NavLink className={css.titleLink} to="/contacts">
+          <div className={css.title}>
+            <LogoIcon />
+            <h1 className={css.titleText}>Contacts</h1>
+          </div>
+        </NavLink>
+      )}
       <div className={css.wrapper}>
         <h2 className={css.heroTitle}>
           All Your Contacts in One Convenient Place
