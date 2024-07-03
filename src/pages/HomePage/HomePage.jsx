7431d91c243b0a7ddc30880bac1e967bba012911
Arsen Akopyan <tigrikpictures@gmail.com>
2024-07-04 02:02:34 +0300

    add title

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,12 +3,23 @@ import css from "./HomePage.module.css";
 import { LogoIcon } from "../../images/icons";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import { useSelector } from "react-redux";
+import clsx from "clsx";
 
 const HomePage = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return (
     <>
+      {!isLoggedIn && (
+        <NavLink className={css.titleLink} to="/">
+          <div className={css.title}>
+            <LogoIcon />
+            <h1 className={clsx(css.titleText, css.mainTitleText)}>
+              Phonebook
+            </h1>
+          </div>
+        </NavLink>
+      )}
       {isLoggedIn && (
         <NavLink className={css.titleLink} to="/contacts">
           <div className={css.title}>
